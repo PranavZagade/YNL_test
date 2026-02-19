2c378e2eab8f1002facda7a6fab40f0d2d7cc5f0
PranavZagade <pranav.zagade@icloud.com>
2026-02-19 07:25:19 -0600

    Fixed all the errors

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['placehold.co'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
   },
 };
 
